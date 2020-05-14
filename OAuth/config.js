c5359aa7a79d63b5b37b4ec9ebcebbd5d77978ef
anjali-001 <43542057+anjali-001@users.noreply.github.com>
2020-05-14 23:39:06 +0530

    OAuth

---
+++ OAuth/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-12345-12345-12345',
+    'mongourl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '711911706219767',
+        clientSecret: 'b9a0711a507a9aae4aec1bf821e02e2a'
+    }
+}
